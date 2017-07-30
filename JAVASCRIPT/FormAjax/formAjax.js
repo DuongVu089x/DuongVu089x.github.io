9d73da8c4ae7823fd664bfec3cd64c7b86d3edcd
DuongVu089x <duonganhvu089x@gmail.com>
2017-07-30 20:05:30 +0700

    finish exam form ajax

--- JAVASCRIPT/FormAjax/formAjax.js
+++ JAVASCRIPT/FormAjax/formAjax.js
@@ -1,28 +1,85 @@
-var txtUserName = document.getElementById('user-name');
-var txtPassword = document.getElementById('password');
-var txtDate = document.getElementById('input-date');
+var chkEmail;
+var chkPassword;
+var chkDate;
+const URL ="http://duonganhvu123.000webhostapp.com/formAjax.php";
 
-(function init() {
-
-})();
-
-function sendData() {
+function sendData(event) {
+    event.preventDefault();
     var xhttp;
     if (window.XMLHttpRequest) {
-        // code for modern browsers
         xhttp = new XMLHttpRequest();
     } else {
-        // code for IE6, IE5
-        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        xhttp = new ActiveXObject('Microsoft.XMLHTTP');
     }
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            var divResultLogin = document.getElementById('login-result');
+            divResultLogin.innerHTML = "";
+            if (this.responseText === 'true') {
+                divResultLogin.innerHTML += "<h1 class='text-success'>Login Success</h1>";
+            } else {
+                divResultLogin.innerHTML += "<h1 class='text-error' style ='display:block'>Login Failed</h1>";
+            }
         }
     };
-    xhttp.open("POST", "formAjax.php", true);
-    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlenconded");
-    xhttp.send("name=" + txtUserName.value + "&pass=" + txtPassword.value + "&birthDay=" + txtDate.value);
+    var txtUserName = document.getElementById('username');
+    xhttp.open("GET", URL+"?name=" + txtUserName.value, true);
+    xhttp.send();
+}
 
-    console.log(12221);
+function checkEmail(event) {
+    event.srcElement.classList = "";
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (!reg.test(event.target.value)) {
+        event.srcElement.classList += "error";
+        document.getElementById('button-submit').disabled = true;
+        document.getElementById('err-username').style.display = "block";
+        chkEmail = false;
+    } else {
+        chkEmail = true;
+        document.getElementById('err-username').style.display = "none";
+        enableButton();
+    }
+}
+
+function checkPassword(event) {
+    // at least one number, one lowercase and one uppercase letter
+    // at least six characters
+    var reg = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    var value = event.target.value;
+    event.srcElement.classList = "";
+
+    if (!reg.test(value)) {
+        event.srcElement.classList += " error";
+        document.getElementById('button-submit').disabled = true;
+        document.getElementById('err-password').style.display = "block";
+        chkPassword = false;
+    } else {
+        chkPassword = true;
+        document.getElementById('err-password').style.display = "none";
+        enableButton();
+    }
+
+}
+
+function checkDate(event) {
+    event.srcElement.classList = "date";
+    var bits = event.target.value.split('/');
+    var d = new Date(bits[2], bits[1] - 1, bits[0]);
+    if (!(d && (d.getMonth() + 1) == bits[1])) {
+        event.srcElement.classList += " error";
+        document.getElementById('button-submit').disabled = true;
+        document.getElementById('err-date').style.display = "block";
+        chkDate = false;
+    } else {
+        chkDate = true;
+        document.getElementById('err-date').style.display = "none";
+        enableButton();
+    }
+}
+
+function enableButton() {
+    if (chkDate && chkPassword && chkDate) {
+        document.getElementById('button-submit').disabled = false;
+    }
 }
--- JAVASCRIPT/FormAjax/formAjax.php
+++ JAVASCRIPT/FormAjax/formAjax.php
@@ -1,3 +1,14 @@
 <?php
-    echo $_GET["name"];
+    header('Access-Control-Allow-Origin: *');  
+    $list_user_name = Array('1231@gmail.com','123456@gmail.com','duonganhvu123@gmail.com');
+    if (isset($_GET['name'])) {
+        $result = false;
+        foreach ($list_user_name as $key => $user_name) {
+            if($user_name === $_GET['name']){
+                $result = true;
+                break;
+            }
+        } 
+        echo $result? "true":"false"; 
+    }
 ?>
